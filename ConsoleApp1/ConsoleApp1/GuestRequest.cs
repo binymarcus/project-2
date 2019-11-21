39aaaacd7f90a1e0b8ef63b648bcc438296bec29
binymarcus <48927315+binymarcus@users.noreply.github.com>
2019-11-21 12:07:30 +0200

    oushing gabi

---
+++ ConsoleApp1/ConsoleApp1/GuestRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GuestRequest
+    {
+      public DateTime EntryDate;
+      public DateTime ReleaseDate;
+      public bool IsApproveed;
+        public override string ToString()
+        {
+            return "Entry Date: " + EntryDate.Year.ToString() +'.' +EntryDate.Month.ToString()+EntryDate.Day.ToString()+" Release Date: " + ReleaseDate.Year.ToString() +ReleaseDate.Month.ToString()+ReleaseDate.Day.ToString()+ " is approved: " + IsApproveed;
+
+        }
+        public GuestRequest()
+        {
+            EntryDate = new DateTime(2020, 1, 1);
+            ReleaseDate = new DateTime(2020, 1, 4);
+            IsApproveed = false;
+         }
+
+
+    }
+}
